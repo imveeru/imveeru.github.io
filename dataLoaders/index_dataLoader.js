348de4b195ea79042449a85f48e5d6c9ddce17e1
imvee <vind.veeru@gmail.com>
2023-08-08 01:47:32 +0530

    Popup for project

--- dataLoaders/index_dataLoader.js
+++ dataLoaders/index_dataLoader.js
@@ -3,27 +3,31 @@ const projects = [
     {
         "id": 1,
         "title": "IntelliML",
-        "subtitle": "AI based AutoML app"
+        "subtitle": "AI based AutoML app",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa",
+        
     },
     {
         "id": 2,
         "title": "Pacemon",
-        "subtitle": "CV based Vehicle monitoring"
+        "subtitle": "CV based Vehicle monitoring",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa",
     },
     {
         "id": 3,
         "title": "Wa-bot",
-        "subtitle": "IoT based robot"
+        "subtitle": "IoT based robot",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa",
     },
     {
         "id": 4,
         "title": "Learn Sign",
-        "subtitle": "AI and CV based game"
+        "subtitle": "AI and CV based game",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa",
     }
 ]
 
-//console.log(projects.length)
-
+//project list
 let temp = document.getElementById("project_template");
 console.log(temp)
 
@@ -32,4 +36,15 @@ for(let i=0;i<projects.length;i++){
     clone.querySelector("#title").textContent=projects[i].title;
     clone.querySelector("#subtitle").textContent=projects[i].subtitle;
     document.getElementById("work_container").appendChild(clone);
+}
+
+//popups
+let popup_temp = document.getElementById("project_popup");
+console.log(popup_temp)
+
+for(let i=0;i<projects.length;i++){
+    let clone = popup_temp.content.cloneNode(true);
+    clone.querySelector("#popup_title").textContent=projects[i].title;
+    clone.querySelector("#popup_content").textContent=projects[i].content;
+    document.getElementById("popup_container").appendChild(clone);
 }
